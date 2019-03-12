59b16a0c4d2529d6890c65127da56e5ede1a1dd0
KoingDev <sea.saingkoing@gmail.com>
2019-03-12 11:52:16 +0900

    [WIP] Solute problem 2: Exclusive Product [Hard]

---
+++ Daily Coding Problem.playground/Pages/Problem2 (ExclusiveProduct).xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+/**
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+*/
+
+
+#warning("TODO: Without using division")
+func exclusiveProduct(of input: [Int]) -> [Int] {
+	return []
+}
+
+func exclusiveProductUsingDivision(of input: [Int]) -> [Int] {
+	
+	var result = [Int]()
+	var prod = 1
+	
+	for number in input {
+		prod *= number
+	}
+	
+	for number in input {
+		result.append(prod / number)
+	}
+	
+	return result
+}
+
+// Using division
+exclusiveProductUsingDivision(of: [1, 2, 3, 4, 5])
+exclusiveProductUsingDivision(of: [3, 2, 1])
